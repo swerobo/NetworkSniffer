ebaead03a4b38534d87ee4f81dba28e311108af8
unknown <swerobo@gmail.com>
2023-03-13 07:10:02 +0100

    First commit

---
+++ NetworkSniffer/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkSniffer.ViewModels
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedViewModel = new SnifferViewModel();
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+    }
+}
